0a0b315580f9d5392f28cdbd10727e8f2c021a54
Valery Efremov <valeriy131100@gmail.com>
2022-01-14 11:35:23 +0300

    Send telegram message for new reviews

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 import requests
+import telegram
 from dotenv import load_dotenv
 from requests.exceptions import ConnectionError, ReadTimeout
 
@@ -41,6 +42,29 @@ def longpoll_dvmn(token):
 if __name__ == '__main__':
     load_dotenv()
     dvmn_token = os.getenv('DVMN_TOKEN')
+    telegram_token = os.getenv('TELEGRAM_TOKEN')
+    chat_id = 363741056
+    telegram_bot = telegram.Bot(token=telegram_token)
+
+    success_message = ('Преподавателю всё понравилось,'
+                       ' можно приступать к следующему уроку\\.')
+
+    failure_message = 'К сожалению, в работе нашлись ошибки\\.'
 
     for event in longpoll_dvmn(dvmn_token):
-        print(event)
+        for review in event['new_attempts']:
+            lesson_title = review['lesson_title']
+            lesson_url = review['lesson_url']
+            is_negative = review['is_negative']
+
+            message = (
+                f'Преподаватель проверил работу '
+                f'[{lesson_title}]({lesson_url})\\.\n\n'
+                f'{failure_message if is_negative else success_message}'
+            )
+
+            telegram_bot.send_message(
+                chat_id=chat_id,
+                text=message,
+                parse_mode='MarkdownV2'
+            )
